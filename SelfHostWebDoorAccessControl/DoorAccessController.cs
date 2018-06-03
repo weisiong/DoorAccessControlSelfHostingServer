f0410a1e93d66ed37c888aa4e4447bc5c7083eca
Richard <wei_siong@yahoo.com>
2018-06-03 20:45:27 +0800

    Add project files.

---
+++ SelfHostWebDoorAccessControl/DoorAccessController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SelfHostWebDoorAccessControl
+{
+    public class DoorAccessController : ApiController
+    {
+        List<string> CardIDs = new List<string> {"669D38E0","669D38E0","04C4554E","840DFA4D","A6BD37E0","54F04F4E","4495544E"}; //"107CCC73","0482554E","D3B4511C",
+
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return CardIDs;
+        }
+
+        // GET api/values/5 
+        public string Get(string id)
+        {
+            var ret = string.Empty;
+            Console.WriteLine($"\r\nReceive --> {id}");
+            if (CardIDs.Contains(id))
+            {
+                Console.WriteLine("ID Found, Access Granted.");
+                ret = "Pass";
+            }
+            else
+            {
+                Console.WriteLine("ID Not Found, Access Denied.");
+                ret = "Fail";
+            }
+            return ret;
+        }
+
+    }
+}
---
+++ SelfHostWebDoorAccessControl/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
+
+namespace SelfHostWebDoorAccessControl
+{
+    public class Program
+    {
+        const string HostIP = "192.168.1.138";
+        const string HostPort = "80";
+
+        static void Main()
+        {
+            string baseAddress = $"http://{HostIP}:{HostPort}/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient
+                HttpClient client = new HttpClient();
+                string reqUri = baseAddress + "api/DoorAccess";
+                var response = client.GetAsync(reqUri).Result;
+
+                //Console.WriteLine(response);
+                Console.WriteLine($"\r\nListening at {reqUri}\r\n");
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
